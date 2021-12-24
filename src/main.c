e3cc89a94ec13a62fcb87e01f091cb68b4b6d65c
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-12-24 21:24:04 +0100

    Use USART in main

--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
+#include <USART.h>
+
+#define USART_BAUD 9600
+
 int main(void) {
+    USART_init(USART_BAUD, BIT_8, DISABLED, BIT_1, TRANSMIT | RECEIVE);
+    USART_send_string("Hello world!\r\n");
+
+    while (1);
+
     return 0;
 }
