355ec25fa2a9340308bfb16e8662fd1a2be9c0b5
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-12-31 10:10:01 +0100

    Make 440Hz tone

--- src/main.c
+++ src/main.c
@@ -1,47 +1,49 @@
-#include <util/delay.h>
+#include <stdint.h>
+#include <avr/io.h>
 
 #include <port.h>
 #include <timer0.h>
-#include <USART.h>
+#include <wave/sin_int8_256.h>
 
-#define USART_BAUD 9600
+/* Tone frequency we want to generate. */
+static const uint16_t frequency = 440;
+
+/*
+ * PWM frequency when timer is running in fast PWM mode and with no prescaling.
+ * Check ATmega328P datasheet section 14.7.3 Fast PWM Mode page 80.
+ */
+static const uint16_t pwm_frequency = F_CPU / 256;
+
+/*
+ * f_out = (f_pwm * step) / (2^counter_size)
+ * step = (f_out * (2^counter_size)) / f_pwm
+ */
+static const uint16_t accumulator_step = frequency * 65536 / pwm_frequency;
+
+static void loop(void);
 
 int main(void) {
-    USART_init(
-        USART_BAUD,
-        USART_CS_8,
-        USART_P_DISABLED,
-        USART_SB_1,
-        USART_D_TRANSMIT | USART_D_RECEIVE
-    );
-
-    USART_send_string("Hello world!\r\n");
-
-    port_d_set_output(PORT_D_PIN_5 | PORT_D_PIN_6);
+    port_d_set_output(PORT_D_PIN_6);
 
     timer0_init(
         TIMER0_WGM_PWM_FF,
         TIMER0_CS_NO_SCALING,
         TIMER0_COM_CLEAR,
-        TIMER0_COM_CLEAR
+        TIMER0_COM_DISABLED
     );
 
-    while (1) {
-        uint8_t i = 0;
-        do {
-            timer0_set_compare_register_a(i);
-            timer0_set_compare_register_b(255 - i);
-            i++;
-            _delay_ms(5);
-        } while (i > 0);
-
-        do {
-            timer0_set_compare_register_a(i);
-            timer0_set_compare_register_b(255 - i);
-            i--;
-            _delay_ms(5);
-        } while (i > 0);
-    }
-
+    while (1) loop();
     return 0;
+}
+
+static void loop(void) {
+    static uint16_t accumulator = 0;
+    static uint8_t value = 0;
+
+    accumulator += accumulator_step;
+    value = 128 + SIN_INT8_256[accumulator >> 8];
+
+    timer0_wait_overflow();
+    timer0_set_compare_register_a(value);
+    timer0_clear_overflow();
 }
