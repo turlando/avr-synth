bf1c81f7fa92da1bc3b6c8ccbfa1ba2ee3bb4279
Tancredi Orlando <tancredi.orlando@gmail.com>
2022-01-08 22:54:43 +0100

    Add pin module, abstraction layer over port module

---
+++ include/pin.h
@@ -0,0 +1,41 @@
+#ifndef PIN_H
+#define PIN_H
+
+enum pin_state {
+    PIN_LOW =  0,
+    PIN_HIGH = 1,
+};
+
+enum pin {
+    PIN_0,  /* Port D, pin 0 */
+    PIN_1,  /* Port D, pin 1 */
+    PIN_2,  /* Port D, pin 2 */
+    PIN_3,  /* Port D, pin 3 */
+    PIN_4,  /* Port D, pin 4 */
+    PIN_5,  /* Port D, pin 5 */
+    PIN_6,  /* Port D, pin 6 */
+    PIN_7,  /* Port D, pin 7 */
+
+    PIN_8,  /* Port B, pin 0 */
+    PIN_9,  /* Port B, pin 1 */
+    PIN_10, /* Port B, pin 2 */
+    PIN_11, /* Port B, pin 2 */
+    PIN_12, /* Port B, pin 3 */
+    PIN_13, /* Port B, pin 4 */
+
+    PIN_A0, /* Port C, pin 0 */
+    PIN_A1, /* Port C, pin 1 */
+    PIN_A2, /* Port C, pin 2 */
+    PIN_A3, /* Port C, pin 3 */
+    PIN_A4, /* Port C, pin 4 */
+    PIN_A5, /* Port C, pin 5 */
+};
+
+void pin_set_output(enum pin pin);
+void pin_set_input(enum pin pin);
+void pin_set_pullup(enum pin pin);
+void pin_set_low(enum pin pin);
+void pin_set_high(enum pin pin);
+enum pin_state pin_get_state(enum pin pin);
+
+#endif /* PIN_H */
---
+++ src/pin.c
@@ -0,0 +1,167 @@
+#include <pin.h>
+
+#include <port.h>
+
+void pin_set_output(const enum pin pin) {
+    switch (pin) {
+        case PIN_0:  port_d_set_output(PORT_D_PIN_0); break;
+        case PIN_1:  port_d_set_output(PORT_D_PIN_1); break;
+        case PIN_2:  port_d_set_output(PORT_D_PIN_2); break;
+        case PIN_3:  port_d_set_output(PORT_D_PIN_3); break;
+        case PIN_4:  port_d_set_output(PORT_D_PIN_4); break;
+        case PIN_5:  port_d_set_output(PORT_D_PIN_5); break;
+        case PIN_6:  port_d_set_output(PORT_D_PIN_6); break;
+        case PIN_7:  port_d_set_output(PORT_D_PIN_6); break;
+
+        case PIN_8:  port_b_set_output(PORT_B_PIN_0); break;
+        case PIN_9:  port_b_set_output(PORT_B_PIN_1); break;
+        case PIN_10: port_b_set_output(PORT_B_PIN_2); break;
+        case PIN_11: port_b_set_output(PORT_B_PIN_3); break;
+        case PIN_12: port_b_set_output(PORT_B_PIN_4); break;
+        case PIN_13: port_b_set_output(PORT_B_PIN_5); break;
+
+        case PIN_A0: port_c_set_output(PORT_C_PIN_0); break;
+        case PIN_A1: port_c_set_output(PORT_C_PIN_1); break;
+        case PIN_A2: port_c_set_output(PORT_C_PIN_2); break;
+        case PIN_A3: port_c_set_output(PORT_C_PIN_3); break;
+        case PIN_A4: port_c_set_output(PORT_C_PIN_4); break;
+        case PIN_A5: port_c_set_output(PORT_C_PIN_5); break;
+    }
+}
+
+void pin_set_input(const enum pin pin) {
+    switch (pin) {
+        case PIN_0:  port_d_set_input(PORT_D_PIN_0); break;
+        case PIN_1:  port_d_set_input(PORT_D_PIN_1); break;
+        case PIN_2:  port_d_set_input(PORT_D_PIN_2); break;
+        case PIN_3:  port_d_set_input(PORT_D_PIN_3); break;
+        case PIN_4:  port_d_set_input(PORT_D_PIN_4); break;
+        case PIN_5:  port_d_set_input(PORT_D_PIN_5); break;
+        case PIN_6:  port_d_set_input(PORT_D_PIN_6); break;
+        case PIN_7:  port_d_set_input(PORT_D_PIN_6); break;
+
+        case PIN_8:  port_b_set_input(PORT_B_PIN_0); break;
+        case PIN_9:  port_b_set_input(PORT_B_PIN_1); break;
+        case PIN_10: port_b_set_input(PORT_B_PIN_2); break;
+        case PIN_11: port_b_set_input(PORT_B_PIN_3); break;
+        case PIN_12: port_b_set_input(PORT_B_PIN_4); break;
+        case PIN_13: port_b_set_input(PORT_B_PIN_5); break;
+
+        case PIN_A0: port_c_set_input(PORT_C_PIN_0); break;
+        case PIN_A1: port_c_set_input(PORT_C_PIN_1); break;
+        case PIN_A2: port_c_set_input(PORT_C_PIN_2); break;
+        case PIN_A3: port_c_set_input(PORT_C_PIN_3); break;
+        case PIN_A4: port_c_set_input(PORT_C_PIN_4); break;
+        case PIN_A5: port_c_set_input(PORT_C_PIN_5); break;
+    }
+}
+
+void pin_set_pullup(const enum pin pin) {
+    switch (pin) {
+        case PIN_0:  port_d_set_pullup(PORT_D_PIN_0); break;
+        case PIN_1:  port_d_set_pullup(PORT_D_PIN_1); break;
+        case PIN_2:  port_d_set_pullup(PORT_D_PIN_2); break;
+        case PIN_3:  port_d_set_pullup(PORT_D_PIN_3); break;
+        case PIN_4:  port_d_set_pullup(PORT_D_PIN_4); break;
+        case PIN_5:  port_d_set_pullup(PORT_D_PIN_5); break;
+        case PIN_6:  port_d_set_pullup(PORT_D_PIN_6); break;
+        case PIN_7:  port_d_set_pullup(PORT_D_PIN_6); break;
+
+        case PIN_8:  port_b_set_pullup(PORT_B_PIN_0); break;
+        case PIN_9:  port_b_set_pullup(PORT_B_PIN_1); break;
+        case PIN_10: port_b_set_pullup(PORT_B_PIN_2); break;
+        case PIN_11: port_b_set_pullup(PORT_B_PIN_3); break;
+        case PIN_12: port_b_set_pullup(PORT_B_PIN_4); break;
+        case PIN_13: port_b_set_pullup(PORT_B_PIN_5); break;
+
+        case PIN_A0: port_c_set_pullup(PORT_C_PIN_0); break;
+        case PIN_A1: port_c_set_pullup(PORT_C_PIN_1); break;
+        case PIN_A2: port_c_set_pullup(PORT_C_PIN_2); break;
+        case PIN_A3: port_c_set_pullup(PORT_C_PIN_3); break;
+        case PIN_A4: port_c_set_pullup(PORT_C_PIN_4); break;
+        case PIN_A5: port_c_set_pullup(PORT_C_PIN_5); break;
+    }
+}
+
+void pin_set_low(const enum pin pin) {
+    switch (pin) {
+        case PIN_0:  port_d_set_low(PORT_D_PIN_0); break;
+        case PIN_1:  port_d_set_low(PORT_D_PIN_1); break;
+        case PIN_2:  port_d_set_low(PORT_D_PIN_2); break;
+        case PIN_3:  port_d_set_low(PORT_D_PIN_3); break;
+        case PIN_4:  port_d_set_low(PORT_D_PIN_4); break;
+        case PIN_5:  port_d_set_low(PORT_D_PIN_5); break;
+        case PIN_6:  port_d_set_low(PORT_D_PIN_6); break;
+        case PIN_7:  port_d_set_low(PORT_D_PIN_6); break;
+
+        case PIN_8:  port_b_set_low(PORT_B_PIN_0); break;
+        case PIN_9:  port_b_set_low(PORT_B_PIN_1); break;
+        case PIN_10: port_b_set_low(PORT_B_PIN_2); break;
+        case PIN_11: port_b_set_low(PORT_B_PIN_3); break;
+        case PIN_12: port_b_set_low(PORT_B_PIN_4); break;
+        case PIN_13: port_b_set_low(PORT_B_PIN_5); break;
+
+        case PIN_A0: port_c_set_low(PORT_C_PIN_0); break;
+        case PIN_A1: port_c_set_low(PORT_C_PIN_1); break;
+        case PIN_A2: port_c_set_low(PORT_C_PIN_2); break;
+        case PIN_A3: port_c_set_low(PORT_C_PIN_3); break;
+        case PIN_A4: port_c_set_low(PORT_C_PIN_4); break;
+        case PIN_A5: port_c_set_low(PORT_C_PIN_5); break;
+    }
+}
+
+void pin_set_high(const enum pin pin) {
+    switch (pin) {
+        case PIN_0:  port_d_set_high(PORT_D_PIN_0); break;
+        case PIN_1:  port_d_set_high(PORT_D_PIN_1); break;
+        case PIN_2:  port_d_set_high(PORT_D_PIN_2); break;
+        case PIN_3:  port_d_set_high(PORT_D_PIN_3); break;
+        case PIN_4:  port_d_set_high(PORT_D_PIN_4); break;
+        case PIN_5:  port_d_set_high(PORT_D_PIN_5); break;
+        case PIN_6:  port_d_set_high(PORT_D_PIN_6); break;
+        case PIN_7:  port_d_set_high(PORT_D_PIN_6); break;
+
+        case PIN_8:  port_b_set_high(PORT_B_PIN_0); break;
+        case PIN_9:  port_b_set_high(PORT_B_PIN_1); break;
+        case PIN_10: port_b_set_high(PORT_B_PIN_2); break;
+        case PIN_11: port_b_set_high(PORT_B_PIN_3); break;
+        case PIN_12: port_b_set_high(PORT_B_PIN_4); break;
+        case PIN_13: port_b_set_high(PORT_B_PIN_5); break;
+
+        case PIN_A0: port_c_set_high(PORT_C_PIN_0); break;
+        case PIN_A1: port_c_set_high(PORT_C_PIN_1); break;
+        case PIN_A2: port_c_set_high(PORT_C_PIN_2); break;
+        case PIN_A3: port_c_set_high(PORT_C_PIN_3); break;
+        case PIN_A4: port_c_set_high(PORT_C_PIN_4); break;
+        case PIN_A5: port_c_set_high(PORT_C_PIN_5); break;
+    }
+}
+
+enum pin_state pin_get_state(const enum pin pin) {
+    switch (pin) {
+        case PIN_0:  return (enum pin_state) port_d_get_pin(PORT_D_PIN_0);
+        case PIN_1:  return (enum pin_state) port_d_get_pin(PORT_D_PIN_1);
+        case PIN_2:  return (enum pin_state) port_d_get_pin(PORT_D_PIN_2);
+        case PIN_3:  return (enum pin_state) port_d_get_pin(PORT_D_PIN_3);
+        case PIN_4:  return (enum pin_state) port_d_get_pin(PORT_D_PIN_4);
+        case PIN_5:  return (enum pin_state) port_d_get_pin(PORT_D_PIN_5);
+        case PIN_6:  return (enum pin_state) port_d_get_pin(PORT_D_PIN_6);
+        case PIN_7:  return (enum pin_state) port_d_get_pin(PORT_D_PIN_6);
+
+        case PIN_8:  return (enum pin_state) port_b_get_pin(PORT_B_PIN_0);
+        case PIN_9:  return (enum pin_state) port_b_get_pin(PORT_B_PIN_1);
+        case PIN_10: return (enum pin_state) port_b_get_pin(PORT_B_PIN_2);
+        case PIN_11: return (enum pin_state) port_b_get_pin(PORT_B_PIN_3);
+        case PIN_12: return (enum pin_state) port_b_get_pin(PORT_B_PIN_4);
+        case PIN_13: return (enum pin_state) port_b_get_pin(PORT_B_PIN_5);
+
+        case PIN_A0: return (enum pin_state) port_c_get_pin(PORT_C_PIN_0);
+        case PIN_A1: return (enum pin_state) port_c_get_pin(PORT_C_PIN_1);
+        case PIN_A2: return (enum pin_state) port_c_get_pin(PORT_C_PIN_2);
+        case PIN_A3: return (enum pin_state) port_c_get_pin(PORT_C_PIN_3);
+        case PIN_A4: return (enum pin_state) port_c_get_pin(PORT_C_PIN_4);
+        case PIN_A5: return (enum pin_state) port_c_get_pin(PORT_C_PIN_5);
+
+        default: return -1;
+    }
+}
